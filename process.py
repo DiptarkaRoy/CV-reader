26ffa47e5e98836b15b5d19fa15acdc112d5a1b6
Diptarka Roy <diptarkaroy2@gmail.com>
2022-07-07 20:31:16 +0530

    Added new CV

--- process.py
+++ process.py
@@ -1,6 +1,6 @@
 import process_pdf as reader
 
-a = reader.tokenise('./Resumes/Resume_of_Diptarka_Roy.pdf')
+a = reader.tokenise('./Resumes/debika_piriya_dharmalingam - debika piriya.pdf')
 print(a)
 b = reader.removeStopWords(a)
 print(b)
